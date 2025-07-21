d2b10e57a04f12da5b1884f155817cd40c7bdf64
Jason Li <jasonmli@outlook.com>
2025-07-21 16:05:48 +0800

    feat(db): prepare for #14, add balance management

---
+++ db/pb_migrations/1753079600_updated_users.js
@@ -0,0 +1,87 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("_pb_users_auth_")
+
+  // update collection data
+  unmarshal({
+    "oauth2": {
+      "mappedFields": {
+        "avatarURL": ""
+      }
+    }
+  }, collection)
+
+  // remove field
+  collection.fields.removeById("file376926767")
+
+  // add field
+  collection.fields.addAt(7, new Field({
+    "hidden": false,
+    "id": "number840409451",
+    "max": null,
+    "min": 0,
+    "name": "total_spent",
+    "onlyInt": true,
+    "presentable": false,
+    "required": false,
+    "system": false,
+    "type": "number"
+  }))
+
+  // add field
+  collection.fields.addAt(8, new Field({
+    "hidden": false,
+    "id": "number2901680126",
+    "max": null,
+    "min": -100000,
+    "name": "balance",
+    "onlyInt": true,
+    "presentable": false,
+    "required": false,
+    "system": false,
+    "type": "number"
+  }))
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("_pb_users_auth_")
+
+  // update collection data
+  unmarshal({
+    "oauth2": {
+      "mappedFields": {
+        "avatarURL": "avatar"
+      }
+    }
+  }, collection)
+
+  // add field
+  collection.fields.addAt(7, new Field({
+    "hidden": false,
+    "id": "file376926767",
+    "maxSelect": 1,
+    "maxSize": 0,
+    "mimeTypes": [
+      "image/jpeg",
+      "image/png",
+      "image/svg+xml",
+      "image/gif",
+      "image/webp"
+    ],
+    "name": "avatar",
+    "presentable": false,
+    "protected": false,
+    "required": false,
+    "system": false,
+    "thumbs": null,
+    "type": "file"
+  }))
+
+  // remove field
+  collection.fields.removeById("number840409451")
+
+  // remove field
+  collection.fields.removeById("number2901680126")
+
+  return app.save(collection)
+})
